026cff1bfa3aa98addfab788106f369f7c5bb447
Simon Fessehaye <simon.fessehaye@iomer.com>
2017-02-17 10:29:52 -0700

    sticky navbar

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Navbar,Nav,NavItem,Glyphicon} from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { Affix } from 'react-overlays';
 import './header.css';
 import banner from './banner.jpg';
 
@@ -8,23 +9,25 @@ class Header extends Component {
     render() {
         return (
             
-                <div>
+                <div >
                     <img className="img-responsive" src={banner} role="presentation" style={{maxHeight: 300,width: '100%'}}/>
-                    <Navbar collapseOnSelect className="mainMenu">
-                        <Navbar.Header >
-                            <Navbar.Toggle />
-                        </Navbar.Header>
-                        <Navbar.Collapse>
-                            <Nav>
-                                <LinkContainer to="/"><NavItem eventKey={0}><Glyphicon glyph="home" /> Home</NavItem></LinkContainer>
-                                <LinkContainer to="/events"><NavItem eventKey={1}><Glyphicon glyph="calendar" /> Events</NavItem></LinkContainer>
-                                <LinkContainer to="/announcements"><NavItem eventKey={2}><Glyphicon glyph="tasks" /> Announcements</NavItem></LinkContainer>
-                                <LinkContainer to="/info"><NavItem eventKey={3}><Glyphicon glyph="alert" /> Information</NavItem></LinkContainer>
-                                {/*<LinkContainer to="/ranking"><NavItem eventKey={4}><Glyphicon glyph="star" /> Rankings</NavItem></LinkContainer>*/}
-                                <LinkContainer to="/location"><NavItem eventKey={5}><Glyphicon glyph="globe" /> Location Finder</NavItem></LinkContainer>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Navbar>
+                    <Affix affixStyle={{width:"100%",top: 0, zIndex:25}} offsetTop={250}>
+                        <Navbar collapseOnSelect className="mainMenu">
+                            <Navbar.Header >
+                                <Navbar.Toggle />
+                            </Navbar.Header>
+                            <Navbar.Collapse>
+                                <Nav>
+                                    <LinkContainer to="/"><NavItem eventKey={0}><Glyphicon glyph="home" /> Home</NavItem></LinkContainer>
+                                    <LinkContainer to="/events"><NavItem eventKey={1}><Glyphicon glyph="calendar" /> Events</NavItem></LinkContainer>
+                                    <LinkContainer to="/announcements"><NavItem eventKey={2}><Glyphicon glyph="tasks" /> Announcements</NavItem></LinkContainer>
+                                    <LinkContainer to="/info"><NavItem eventKey={3}><Glyphicon glyph="alert" /> Information</NavItem></LinkContainer>
+                                    {/*<LinkContainer to="/ranking"><NavItem eventKey={4}><Glyphicon glyph="star" /> Rankings</NavItem></LinkContainer>*/}
+                                    <LinkContainer to="/location"><NavItem eventKey={5}><Glyphicon glyph="globe" /> Location Finder</NavItem></LinkContainer>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Navbar>
+                    </Affix >
                 </div>
               
         );
