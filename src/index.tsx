8e0c14744c12d198d8c7ba09078e854e9bf1fbbc
fessehay <fessehay@ualberta.ca>
2018-02-12 02:34:14 -0700

    cleaning files

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import Layout from "./layout";
-import Main from "./container";
-import Announcements from "./container/announcement";
-import Events from "./container/events";
-import Info from "./container/information";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Layout from './layout';
+import Main from './container';
+import Announcements from './container/announcement';
+import Events from './container/events';
+import Info from './container/information';
+import * as registerObserver from 'react-perf-devtool';
 import {
     BrowserRouter as Router,
     Route
-} from "react-router-dom";
+} from 'react-router-dom';
 
-var registerObserver = require('react-perf-devtool');
 registerObserver();
 
 ReactDOM.render(
     <Router>
       <Layout>
-        <Route exact path="/" component={Main}/>
+        <Route exact={true} path="/" component={Main}/>
         <Route path="/announcements" component={Announcements}/>
         <Route path="/events" component={Events}/>
         <Route path="/info" component={Info}/>
