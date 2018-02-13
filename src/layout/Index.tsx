e2bac2978ab80800952c7c82b20bf2d70fc99568
fessehay <fessehay@ualberta.ca>
2018-02-13 01:15:27 -0700

    tslint quotes

--- src/layout/Index.tsx
+++ src/layout/Index.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
+import * as React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
 export default class Layout extends React.Component<{}, {}> {
     
