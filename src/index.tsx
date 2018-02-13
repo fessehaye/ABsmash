03c06a6687cc49c350413bebd2779a5527664770
fessehay <fessehay@ualberta.ca>
2018-02-13 01:12:43 -0700

    cleaned tslint

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Layout from './layout';
 import Main from './container';
-import Announcements from './container/announcement';
-import Events from './container/events';
-import Info from './container/information';
+import Announcements from './container/Announcement';
+import Events from './container/Events';
+import Information from './container/Information';
 import * as registerObserver from 'react-perf-devtool';
 import {
     BrowserRouter as Router,
@@ -19,7 +19,7 @@ ReactDOM.render(
         <Route exact={true} path="/" component={Main}/>
         <Route path="/announcements" component={Announcements}/>
         <Route path="/events" component={Events}/>
-        <Route path="/info" component={Info}/>
+        <Route path="/info" component={Information}/>
       </Layout>
   </Router>,
     document.getElementById('root') as HTMLElement
